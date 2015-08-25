5aac6265f4c1177cc29684944dd7c71e4029dd7d
Daniel Pereira <pereiradpg@yahoo.ca>
2015-08-25 14:25:39 -0400

    Update: Added contacts.erb and linked to it within crm.rb

--- crm.rb
+++ crm.rb
@@ -1,5 +1,12 @@
 require 'sinatra'
 
-get '/' do
-	"Main Menu"
+get '/' do		# Root directory
+	@crm_app_name = "My CRM"	# Instance variable containing application name that can be read by index.erb
+	erb :index	# Refers to the main page, index.erb
+end
+
+
+# Refers to contacts.erb
+get '/contacts' do
+	erb :contacts
 end
