96d574d64a527cca91f8beadc8c0103a10c3f231
Daniel Pereira <pereiradpg@yahoo.ca>
2015-08-25 15:25:57 -0400

    Update: Created route for new contact, and new_contact.erb.

--- crm.rb
+++ crm.rb
@@ -2,6 +2,8 @@ require 'sinatra'
 require_relative 'contact'
 require_relative 'rolodex'
 
+$rolodex = Rolodex.new # Set a global variable to allow acces from each action in Sinatra
+
 get '/' do		# Root directory
 	@crm_app_name = "My CRM"	# Instance variable containing application name that can be read by index.erb
 	erb :index	# Refers to the main page, index.erb
@@ -10,17 +12,11 @@ end
 
 # Refers to contacts.erb
 get '/contacts' do
-	# Fake data for testing
-	@contacts = []
-	@contacts << Contact.new(1000, "Yehuda", "Katz", "yehuda@example.com", "Developer")
-	@contacts << Contact.new(1001, "Mark", "Zuckerberg", "mark@facebook.com", "CEO")
- 	@contacts << Contact.new(1002, "Sergey", "Brin", "sergey@google.com", "Co-Founder")
-
 	erb :contacts
 end
 
 # Add a new contact
-get 'contacts/new' do
-	# insert page ref here
+get '/contacts/new' do
+	erb :new_contact
 end
 
